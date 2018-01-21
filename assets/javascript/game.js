7d829f8c10e04dda658a091607ef5a429935fb61
Yvette <Yvette@macs-MacBook-Air.local>
2018-01-20 23:08:59 -0800

    Added functions to reset game without refreshing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,45 +3,66 @@ $(document).ready(function() {
 	var wins = 0;
 	var losses = 0;
 
-	var targetNumber = Math.floor(Math.random() * 120) + 19;
+	var targetNumber = 0;
 	var currentTotal = 0;
 
 	var crystalArray = ["assets/images/gemRed.svg", "assets/images/gemGreen.svg","assets/images/gemBlue.svg","assets/images/gemYellow.svg"];
-
-//Assigning crystalValues randomly to each of 4 crystals
-	for(var i = 0; i < crystalArray.length; i++) {
-		var imageCrystal = $("<img>");
-
-		imageCrystal.addClass("crystal-size");
-
-		imageCrystal.attr("src", crystalArray[i]);
-
-		var crystalOptions = Math.floor(Math.random() * 12) + 1;
-		imageCrystal.attr("data-crystalvalue", crystalOptions);
-
-		$("#crystals").append(imageCrystal);
-
-		document.querySelector("#targetValue").innerHTML = targetNumber;
-	}
-
-	$(".crystal-size").on("click", function() {
+	
+	newValues();
+	startGame();
+	
+	function startGame() {
+		$(".crystal-size").on("click", function() {
 		var crystalValue = ($(this).attr("data-crystalvalue"));
 		crystalValue = parseInt(crystalValue);
 
 		currentTotal += crystalValue;
-		document.querySelector("#total").innerHTML = currentTotal;
+		$("#total").html(currentTotal);
 
 		if(currentTotal === targetNumber) {
 			wins++;
 			document.querySelector("#wins").innerHTML = wins;
+			reset();
+			newValues();
+			startGame();
 		}
 
 		else if(currentTotal > targetNumber) {
 			losses++;
-			document.querySelector("#losses").innerHTML = losses;
+			$("#losses").html(losses);
+			reset();
+			newValues();
+			startGame();
+		}
+		});
+	};
+
+	function newValues() {
+		for(var i = 0; i < crystalArray.length; i++) {
+			targetNumber = Math.floor(Math.random() * 120) + 19;
+			var crystalOptions = Math.floor(Math.random() * 12) + 1;
+			var imageCrystal = $("<img>");
+
+			imageCrystal.addClass("crystal-size");
+
+			imageCrystal.attr("src", crystalArray[i]);
+
+			imageCrystal.attr("data-crystalvalue", crystalOptions);
+
+			$("#crystals").append(imageCrystal);
+
+			$("#targetValue").html(targetNumber);
+		}
+	};
+
+	function reset() {
+		if(wins > 0 || losses > 0) {
+			currentTotal = 0;
+			$("#total").html("0");
+			$("#crystals").empty();
 		}
 
-	});
+	};
 });
 
 
