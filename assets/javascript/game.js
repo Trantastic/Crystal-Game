468872498b25cc11381f5ae5c31dcac643945d41
Yvette <Yvette@macs-MacBook-Air.local>
2018-01-22 08:27:11 -0800

    Crystal values change with each reset, but there are still duplicates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,84 +1,85 @@
 $(document).ready(function() {
 
-	var wins = 0;
-	var losses = 0;
+    var wins = 0;
+    var losses = 0;
 
-	var targetNumber = 0;
-	var currentTotal = 0;
+    var targetNumber = 0;
+    var currentTotal = 0;
 
-	var crystalArray = ["assets/images/gemRed.svg", "assets/images/gemGreen.svg","assets/images/gemBlue.svg","assets/images/gemYellow.svg"];
-	
-	newValues();
-	startGame();
-	
-	function startGame() {
-		$(".crystal-size").on("click", function() {
-		var crystalValue = ($(this).attr("data-crystalvalue"));
-		crystalValue = parseInt(crystalValue);
+    var crystalArray = ["assets/images/gemRed.svg", "assets/images/gemGreen.svg", "assets/images/gemBlue.svg", "assets/images/gemYellow.svg"];
 
-		currentTotal += crystalValue;
-		$("#total").html(currentTotal);
+    var randomValues = [];
 
-		if(currentTotal === targetNumber) {
-			wins++;
-			document.querySelector("#wins").innerHTML = wins;
-			reset();
-			newValues();
-			startGame();
-		}
+    randomizeCrystals();
+    newValues();
+    startGame();
 
-		else if(currentTotal > targetNumber) {
-			losses++;
-			$("#losses").html(losses);
-			reset();
-			newValues();
-			startGame();
-		}
-		});
-	};
+    function startGame() {
+        $(".crystal-size").on("click", function() {
+            var crystalValue = ($(this).attr("data-crystalvalue"));
+            crystalValue = parseInt(crystalValue);
 
-	function newValues() {
-		for(var i = 0; i < crystalArray.length; i++) {
-			targetNumber = Math.floor(Math.random() * 120) + 19;
-			var crystalOptions = Math.floor(Math.random() * 12) + 1;
-			var imageCrystal = $("<img>");
+            currentTotal += crystalValue;
+            $("#total").html(currentTotal);
 
-			imageCrystal.addClass("crystal-size");
+            if (currentTotal === targetNumber) {
+                wins++;
+                document.querySelector("#wins").innerHTML = wins;
+                reset();
+                randomizeCrystals();
+                newValues();
+                startGame();
+            } else if (currentTotal > targetNumber) {
+                losses++;
+                $("#losses").html(losses);
+                reset();
+            	randomizeCrystals();
+                newValues();
+                startGame();
+            }
+        });
+    };
 
-			imageCrystal.attr("src", crystalArray[i]);
+    function randomizeCrystals() {
+        while(randomValues.length < crystalArray.length) {
+            var crystalOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+            
+            crystalOptions.sort(function(a, b) {
+                return 0.5 - Math.random()
+            });
+            
+            randomValues.push(crystalOptions.splice(0, 1));
+        }
+       	console.log(crystalOptions);
+    };
 
-			imageCrystal.attr("data-crystalvalue", crystalOptions);
+    function newValues() {
+        for (var i = 0; i < crystalArray.length; i++) {
+            targetNumber = Math.floor(Math.random() * 101) + 19;
 
-			$("#crystals").append(imageCrystal);
+            var imageCrystal = $("<img>");
 
-			$("#targetValue").html(targetNumber);
-		}
-	};
+            imageCrystal.addClass("crystal-size");
 
-	function reset() {
-		if(wins > 0 || losses > 0) {
-			currentTotal = 0;
-			$("#total").html("0");
-			$("#crystals").empty();
-		}
+            imageCrystal.attr("src", crystalArray[i]);
 
-	};
+            imageCrystal.attr("data-crystalvalue", randomValues[i]);
+
+            console.log(randomValues);
+
+            $("#crystals").append(imageCrystal);
+
+            $("#targetValue").html(targetNumber);
+        }
+    };
+
+    function reset() {
+        if (wins > 0 || losses > 0) {
+            currentTotal = 0;
+            $("#total").html("0");
+            $("#crystals").empty();
+            randomValues = [];
+        }
+
+    };
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
